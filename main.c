e79dbd84daea0f10e17c3747abfedbb83f94c95e
JoaoFLM <lopesmonteirojoaofelipe@gmail.com>
2026-01-09 18:25:54 -0300

    Make the linked list a double linked list

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 struct Node{
     int value;
+    struct Node* anterior; 
     struct Node* proximo;
 };
 
@@ -10,18 +11,19 @@ void CreateNodeList(int value, struct Arena* arena, struct Node** head){
     struct Node* new_node = ArenaAlloc(arena, sizeof(struct Node));
     new_node->value = value;
     new_node->proximo = NULL;
+    new_node->anterior = NULL;
 
     if(*head == NULL){
 	*head = new_node; 
 	return;
     }
 
-    struct Node* temp = *head;
+    struct Node* temp = *head; 
     while(temp->proximo != NULL){
 	temp = temp->proximo;
     }
-
     temp->proximo = new_node;
+    new_node->anterior = temp;
 
     return;
 }
@@ -41,6 +43,17 @@ void RemoveNodeList(struct Node** head){
     return;
 }
 
+struct Node* GetNodeById(struct Node** node, int id){
+    struct Node* temp = *node;
+    for(int i = 0; i < id; i++){
+	if(temp->proximo != NULL){
+	    temp = temp->proximo;
+	}
+    }
+
+    return temp;
+}
+
 int main(){
     const size_t PAGE_SIZE = (size_t)sysconf(_SC_PAGESIZE);
     
@@ -53,19 +66,33 @@ int main(){
     CreateNodeList(3, arena, &node);
 
     struct Node* temp = node;
-    for(int i = 0; temp != NULL; i++){
+    
+    int i = 0;
+   
+    while (temp != NULL) {
 	printf("Nó número: %d\nvalor: %d\n", i, temp->value);
+	i++;
+	if (temp->proximo == NULL) break;
 	temp = temp->proximo;
     }
 
-	printf("--------------------------\n");
+    printf("--------------------------\n");
 
-    RemoveNodeList(&node);
-
-    temp = node;
-    for(int i = 0; temp != NULL; i++){
+    while (temp != NULL) {
 	printf("Nó número: %d\nvalor: %d\n", i, temp->value);
-	temp = temp->proximo;
+	temp = temp->anterior;
+	i--;
+    }
+
+    printf("--------------------------\n");
+    
+    int index = 0;
+
+    temp = GetNodeById(&node, index);
+
+    for(int i = index; temp != NULL; i--){
+	printf("Nó número: %d\nvalor: %d\n", i, temp->value);
+	temp = temp->anterior;
     }
 
     ArenaRelease(arena);
